ace301e42293bbaae6c500abc621e023f2cc11c8
Gurpreet90015 <135943248+Gurpreet90015@users.noreply.github.com>
2024-02-21 16:23:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
+let inpt1=document.querySelector("#fname")
+let inpt2=document.querySelector("#lname")
+let btn=document.querySelector(".submit")
 
+btn.addEventListener("click" , getFormvalue )
+
+function getFormvalue(eventDetails) {
+eventDetails.preventDefault()
+
+console.log(inpt1.value)
+console.log(inpt2.value)
+
+alert(`${inpt1.value} ${inpt2.value}`)
+	
 }
